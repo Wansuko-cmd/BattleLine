a3c59b4009c0a40829d4f5bb2875a1cca29b032a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-24 22:07:27 +0900

    [add] Troopを追加

---
+++ domain/src/commonMain/kotlin/com/wsr/Troop.kt
@@ -0,0 +1,28 @@
+package com.wsr
+
+data class Troop(
+    val position: Position,
+    val color: Color,
+)
+
+enum class Position(val power: Int) {
+    Elephants(10),
+    Chariots(9),
+    HeavyCavalry(8),
+    LightCavalry(7),
+    Hypaspists(6),
+    Phalangists(5),
+    Hoplites(4),
+    Javalineers(3),
+    Peltasts(2),
+    Skirmishers(1);
+}
+
+enum class Color {
+    Red,
+    Yellow,
+    Green,
+    Blue,
+    Orange,
+    Purple;
+}
